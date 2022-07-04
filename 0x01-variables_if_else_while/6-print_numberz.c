4a65a798a0c0a250dcd64af0caebf4ca4b573f3e
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-04 11:57:18 -0700

    C program that prints all single digit numbers of base 10 using int variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - using the int variable to print 0-9
+ *
+ * Return: 0 always which means success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9' num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
