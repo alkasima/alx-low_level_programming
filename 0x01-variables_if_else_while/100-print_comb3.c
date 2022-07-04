ca292417d93559e04cd59374319d2a790936ba10
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-04 13:25:07 -0700

    C program to prints all possbile different combinations of two digits separated by commas

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print the combination of digits
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int ch;
+	int j;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if(ch < j)
+			{
+				putchar(ch);
+				putchar(j);
+
+				if (ch != '5' || (c == '5' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
