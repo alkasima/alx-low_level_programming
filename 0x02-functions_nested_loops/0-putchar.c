888ed4510e0b9b699ab4104b17872220d841ae7b
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-05 09:09:20 -0700

    C program to prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - C program to print _putchar
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	char c = "_putchar";
+
+	printf(c);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Nested loops and use of functions in c programming
