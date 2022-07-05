e92ee5fad79ea00decaae7bdfc4a1787824b5e25
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-05 09:57:32 -0700

    C program to prints alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - program to print alphabet using _putchar
+ *
+ * Return: 0 always success
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
