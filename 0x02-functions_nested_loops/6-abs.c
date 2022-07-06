2870b4bb32c4cde8854c567a948a696ce16ed5b3
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-06 09:58:03 -0700

    C program function that computers the absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "min.h"
+
+/**
+ * _abs - compute the absolute value of integer
+ *
+ * @i: integer taht is abosulute
+ * Return: the absolute value of an integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
