8ca91e543515b7da482ac7fcd19e5b2e355203c8
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-07 20:29:54 -0700

    test positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -test the positive_or_negative function to return if number is negative or positive
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+C debuggin project
