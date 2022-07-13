ed1467cb3997f0d959bfc1fdd2dbbb1bc1110bf1
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-12 19:43:39 -0700

    Function to print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function to print a string
+ * @str: string variable
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
