9c21ddf35182acd6f58832cd123bec7275557df9
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-07-13 17:26:46 -0700

    Function that copies the string pointed to by src including the terminating null

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,14 +12,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int j;
+
 	j = 0;
 
 	while (*(src + j) != '\0')
 	{
 		*(dest + j) = *(src + j);
 		j++;
-	}
-	
+	}	
 	*(dest + j) = '\0';
+	
 	return (dest);
 }
